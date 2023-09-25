6d089972a2a2c102bcd97458d6c59865e2be1403
cColds <103373668+cColds@users.noreply.github.com>
2023-09-25 07:06:13 -0500

    Setup app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">
+        Blog editor for admins to manage posts.
+      </h1>
+    </>
+  );
+}
+
+export default App;
